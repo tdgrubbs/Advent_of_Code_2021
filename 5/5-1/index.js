e631b2473fc0136b207d562bbe2487f606e2f6bb
Tanner <32615341+tdgrubbs@users.noreply.github.com>
2022-05-30 09:55:28 -0500

    Create index.js

---
+++ 5/5-1/index.js
@@ -0,0 +1,62 @@
+const input = require('./input.js');
+
+const inputArr = input.split('\n')
+  .map((x) => x.split(' -> '))
+  .map(([...x]) => [x[0].split(','), x[1].split(',')])
+  .map((x) =>
+    [
+      [parseInt(x[0][0]), parseInt(x[0][1])],
+      [parseInt(x[1][0]), parseInt(x[1][1])]
+    ])
+
+const inputHV = inputArr.filter((x) => x[0][0] === x[1][0] || x[0][1] === x[1][1])
+const range =
+  [
+    inputHV.flat(Infinity).sort((a, b) => a - b)[0],
+    inputHV.flat(Infinity).sort((a, b) => b - a)[0]
+  ]
+
+const indexPlacement = (arr, gridWidth) => {
+
+  const lineIndexes = []
+  const lineArr = new Array(1000000).fill(0)
+
+  arr.forEach((x) => {
+    const [[x1, y1], [x2, y2]] = x
+
+    if (x1 > x2) {
+      lineIndexes.push(Array.from(
+        { length: x1 - x2 + 1 },
+        (_, i) => i < 1 ? i + ((y2 * gridWidth) + x2)
+          : ((y2 * gridWidth) + x2) + i))
+    } else if (x2 > x1) {
+      lineIndexes.push(Array.from(
+        { length: x2 - x1 + 1 },
+        (_, i) => i < 1 ? i + ((y1 * gridWidth) + x1)
+          : ((y1 * gridWidth) + x1) + i))
+    } else if (y1 > y2) {
+      lineIndexes.push(Array.from(
+        { length: y1 - y2 + 1 },
+        (_, i) => i < 1 ? i + ((y2 * gridWidth) + x2)
+          : (((y2 * gridWidth) + x2) + i) + ((i * gridWidth) - i)))
+    } else if (y2 > y1) {
+      lineIndexes.push(Array.from(
+        { length: y2 - y1 + 1 },
+        (_, i) => i < 1 ? i + ((y1 * gridWidth) + x1)
+          : (((y1 * gridWidth) + x1) + i) + ((i * gridWidth) - i)))
+    }
+  })
+
+  lineIndexes.forEach((x) => {
+    [...x].forEach((x) => {
+      lineArr[x] === 0 ? lineArr[x] = 1 : lineArr[x]++
+    })
+  })
+
+  const overlap = lineArr.filter((x) => x >= 2)
+
+  return overlap.length
+}
+
+
+indexPlacement(inputHV, 1000)
